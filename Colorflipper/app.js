a2638a746819997c8247bb88a73ab2544c61366b
unknown <Sumesh1712@gmail.com>
2023-02-13 13:52:11 +0530

    Added loop and math function for ColorFlipper

--- Colorflipper/app.js
+++ Colorflipper/app.js
@@ -1,1 +1,10 @@
-const colors = ["green", "red", "rgba(133,122,200)", "#f15025"];
+const colors = ["green", "red", "rgba(133,122,200)", "#f15025", "brown","yellow"];
+
+const btn = document.getElementById("btn")
+const color= document.querySelector(".color")
+
+btn.addEventListener("click", function(){
+    let newColor =  Math.floor(Math.random()*colors.length)
+    document.body.style.background = colors[newColor]
+    color.textContent = colors[newColor]
+})
--- Colorflipper/hex.js
+++ Colorflipper/hex.js
@@ -1,1 +1,19 @@
 const hex = [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, "A", "B", "C", "D", "E", "F"];
+
+const btn = document.getElementById("btn")
+const color= document.querySelector(".color")
+
+btn.addEventListener("click", function(){
+    let result = 0
+
+    for(let i=0; i< 5; i++){
+        
+       
+        result += hex[Math.floor(Math.random()* hex.length)]
+    }
+    let hexColor = `#${result}`
+    document.body.style.background = hexColor
+    color.textContent = hexColor
+
+    
+})
