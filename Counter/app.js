ecba6491618034f1bd1ebe09c8beb97aef485e4f
unknown <Sumesh1712@gmail.com>
2023-02-16 16:23:12 +0530

    improving the JS code

--- Counter/app.js
+++ Counter/app.js
@@ -1,21 +1,21 @@
-let valuedisplay = document.querySelector("#value")
-let reset = document.querySelector(".reset")
-let increase = document.querySelector(".increase")
-let decrease = document.querySelector(".decrease")
+let valueDisplay = document.querySelector("#value")
+let btns = document.querySelectorAll(".btn")
+
 let num=0
 
-increase.addEventListener("click",function(){
-    num +=1
-    valuedisplay.textContent = num
-    
-})
 
-reset.addEventListener("click", function(){
-    valuedisplay.textContent = num = 0
-})
-
-decrease.addEventListener("click",function(){
-    num -=1
-    valuedisplay.textContent = num
-    
+btns.forEach(function (btn)
+{
+btn.addEventListener("click", function(e){
+   let action =  e.currentTarget.classList
+        if (action.contains("decrease")){
+                num--
+        } else if (action.contains("increase")){
+            num++
+        }
+          else {
+            num = 0
+          }
+          valueDisplay.textContent=num
+} )
 })
