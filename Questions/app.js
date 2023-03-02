28e4a1b0219845cec1a80a978dc9b8ef12b7b867
unknown <Sumesh1712@gmail.com>
2023-03-02 13:36:03 +0530

    Added toggle function to show and hide answers of questions

--- Questions/app.js
+++ Questions/app.js
@@ -1,2 +1,12 @@
 //using selectors inside the element
 // traversing the dom
+const article = document.querySelector("article")
+const btns = document.querySelectorAll(".question-btn")
+
+btns.forEach(function (btn){
+    btn.addEventListener("click", function(e){
+     const btnfunction = e.currentTarget.parentElement.parentElement
+        console.log(btnfunction)   
+     btnfunction.classList.toggle("show-text")
+    })
+})
