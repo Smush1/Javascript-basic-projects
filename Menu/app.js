d04bc76c98533b7ddff22f9da2492a8e943bde78
unknown <Sumesh1712@gmail.com>
2023-03-14 16:14:56 +0530

    Created buttons for different menus

--- Menu/app.js
+++ Menu/app.js
@@ -73,26 +73,39 @@ const menu = [
     },
   ];
   
+const filterBtns = document.querySelectorAll(".filter-btn")
+
+
 
 const sectionCenter = document.querySelector(".section-center")
-
+//load items
 window.addEventListener("DOMContentLoaded", function(){
-    console.log("shake and bake")
-    let displayMenu = menu.map(function(item){
-       return `<article class="menu-item">
-        <img src=${item.img} class="photo" alt=${item.title}>
-        <div class="item-info">
-            <header>
-                <h4>${item.title}</h4>
-                <h4 class="price"> $${item.price}</h4>
-            </header>
-            <p class="item-text">
-                ${item.desc}
-            </p>
-        </div>
-    </article>`
-    })
-    displayMenu = displayMenu.join("")
-   console.log(displayMenu)
-   sectionCenter.innerHTML = displayMenu
+    displayMenuItems(menu)
 })
+
+//filter Buttons
+filterBtns.forEach(function(btn){
+  btn.addEventListener("click",function(e){
+    console.log(e.currentTarget.dataset)
+    
+  })
+})
+
+function displayMenuItems(menuItems){
+  let displayMenu = menuItems.map(function(item){
+    return `<article class="menu-item">
+     <img src=${item.img} class="photo" alt=${item.title}>
+     <div class="item-info">
+         <header>
+             <h4>${item.title}</h4>
+             <h4 class="price"> $${item.price}</h4>
+         </header>
+         <p class="item-text">
+             ${item.desc}
+         </p>
+     </div>
+ </article>`
+ })
+ displayMenu = displayMenu.join("")
+sectionCenter.innerHTML = displayMenu
+}
