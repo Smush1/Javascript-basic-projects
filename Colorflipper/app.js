708a026667c06f5276eb663206b578dd725b8b93
Sumesh Kumar <sumesh1712@gmail.com>
2024-02-17 14:30:12 +0530

    Created async repo for callbacks, promises and async/await

--- Colorflipper/app.js
+++ Colorflipper/app.js
@@ -6,6 +6,5 @@ const color= document.querySelector(".color")
 btn.addEventListener("click", function(){
     let newColor =  Math.floor(Math.random()*colors.length);
     document.body.style.background = colors[newColor]
-    console.log(newColor)
     color.textContent = colors[newColor]
 })
